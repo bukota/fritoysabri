8bc231a6bf822507e88052a4aa1a5a083f4c26a5
Nicolas Bukstein <nbuksteingutenmajer@orbitz.com>
2015-11-02 11:06:27 +0100

    First commit

---
+++ Fritoysabri/Fritoysabri/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fritoysabri
+//
+//  Created by Nicolas Bukstein on 2/11/15.
+//  Copyright © 2015 Nicolas Bukstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
