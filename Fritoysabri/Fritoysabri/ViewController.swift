47d2cd508528611810fa43c5c6dede3bfc02824f
Nicolas Bukstein <nbuksteingutenmajer@orbitz.com>
2015-11-02 15:05:03 +0100

    agregue imagen de frito y sabri y mejore los métodos de creación de botones en el viewcontroller

--- Fritoysabri/Fritoysabri/ViewController.swift
+++ Fritoysabri/Fritoysabri/ViewController.swift
@@ -12,18 +12,45 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let loginButton = FBSDKLoginButton.init()
-        loginButton.center = self.view.center;
-        self.view.addSubview(loginButton)
+        let loginButton = UIButton.init(type: UIButtonType.Custom)
+        loginButton.backgroundColor = UIColor.blueColor()
+        loginButton.frame = CGRect.init(x: 0, y: 0, width: 180, height: 40)
+        loginButton.center = view.center
+        loginButton.setTitle("Play ", forState: UIControlState.Normal)
         
-        // Do any additional setup after loading the view, typically from a nib.
+        loginButton.addTarget(self, action:Selector("loginButton"), forControlEvents: UIControlEvents.TouchUpInside)
+        view.addSubview(loginButton)
+        
+    }
+    
+    func createPlayAudioButton() {
+        
+    }
+    
+    
+    func createFacebookLoginButton () {
+        let loginButton = UIButton.init(type: UIButtonType.Custom)
+        loginButton.backgroundColor = UIColor.blueColor()
+        loginButton.frame = CGRect.init(x: 0, y: 0, width: 180, height: 40)
+        loginButton.center = view.center
+        loginButton.setTitle("Login with facebook!", forState: UIControlState.Normal)
+        
+        loginButton.addTarget(self, action:Selector("loginButton"), forControlEvents: UIControlEvents.TouchUpInside)
+        view.addSubview(loginButton)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func loginButton() {
+        let loginManager = FBSDKLoginManager.init()
+        loginManager.logInWithReadPermissions(["public_profile"], fromViewController: self) { (result :FBSDKLoginManagerLoginResult!,error: NSError!) -> Void in
+            if (error != nil) {
+                print("Error")
+            }else if result.isCancelled{
+                print("Canceled")
+            }else{
+                print("token:\(result.token.tokenString)")
+            }
+        }
     }
-
 
 }
 
